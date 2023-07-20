7e6803f0d13e56ac46ff5fc97cd4f22f64340642
AnastasiiaDia <anastasiia.iv@gmail.com>
2023-07-20 18:31:45 +0200

    fin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,15 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 
 const ulEl = document.querySelector(".gallery");
 
-// ulEl.addEventListener("click", onClick);
-
-let lightbox = new SimpleLightbox(".gallery a", {
-  captions: true,
-  captionsData: "alt",
-  captionPosition: "bottom",
-  captionDelay: 250,
-});
-
 SetToGallery(CreateMarkup(galleryItems));
 
 function CreateMarkup(galleryItems) {
@@ -25,28 +16,9 @@ function CreateMarkup(galleryItems) {
 function SetToGallery(gallery) {
   ulEl.innerHTML = gallery;
 }
-
-// function onClick(evt) {
-//   evt.preventDefault();
-//   if (evt.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   scale(evt);
-// }
-
-// function scale(evt) {
-
-//   const instance = basicLightbox.create(
-//     `<img src="${evt.target.dataset.source}" width="800" height="600">`
-//   );
-
-//   ulEl.addEventListener(
-//     "keydown",
-//     (evt) => {
-//       if (evt.code === "Escape") {
-//         instance.close();
-//       }
-//     },
-//     { once: true }
-//   );
-// }
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
